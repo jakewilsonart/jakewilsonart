965443fe71b0ce3fdc3bcb7ce0306ee3c55b7c91
jakewilsonart <103470119+jakewilsonart@users.noreply.github.com>
2022-04-11 21:52:00 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-
-const hamburger = document.getElementsByClassName('hamburger')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-hamburger.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
